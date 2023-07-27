ef757a52f55980f9b3f7593067d75580cb2c0ae7
sami <samif3443@gmail.com>
2023-07-27 11:26:10 -0700

    Added

--- react-redux-dev/src/components/Navbar.js
+++ react-redux-dev/src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <>
       <nav>
         <div className='nav-center'>
-          <h3>redux toolkit</h3>
+          <h3>Redux toolkit</h3>
           <div className='nav-container'>
             <CartIcon />
             <div className='amount-container'>
