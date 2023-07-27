d09252afd41250b07044b37e083b66d3a1888ff8
sami <samif3443@gmail.com>
2023-07-27 11:25:46 -0700

    Added

--- react-redux-dev/src/components/Modal.js
+++ react-redux-dev/src/components/Modal.js
@@ -7,7 +7,7 @@ const Modal = () => {
   return (
     <aside className='modal-container'>
       <div className='modal'>
-        <h4>remove all items from your shopping cart?</h4>
+        <h4>Remove all items from your shopping cart?</h4>
         <div className='btn-container'>
           <button
             type='button'
